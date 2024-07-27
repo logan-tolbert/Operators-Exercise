9789cd1677ac6ed3d8bd2d598477a26923845edc
logan-tolbert <logo88205+git@gmail.com>
2024-07-27 11:48:43 -0500

    complete exercise 2

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,32 +2,54 @@
 {
     public class Program
     {
+        // Ex2a. get radius from user
+        public static double GetRadius()
+        {
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            return radius;
+        }
+
+
+        // Ex2b. define AreaOfCircle method
+        public static double AreaOfCircle(double radius)
+        {
+            return (Math.PI * radius * radius);
+        }
+
+
         static void Main(string[] args)
         {
-            // Exercise 1
-            // 1. Create 2 integer variables
+
+            // Ex1a. create 2 integer variables
             int a = 17;
             int b = 4;
 
-            // 2. write equations using addition, subtraction, multiplication, divison, and modulous
+            // Ex1b. write equations using addition, subtraction, multiplication, divison, and modulous
             var sum = a + b;
             var difference = a - b;
             var product = a * b;
             var quotient = (double) a / b;
             var remainder = (double) a % b;
 
-           
-            // 3. create 2 interger variables for division
+            // Ex1c. create 2 interger variables for division
             int quotient2 = a / b;
             int remainder2 = a % b;
 
-            // 4. output results with formated string
-            Console.WriteLine($"The sum of {a} + {b} is {sum}\n");
-            Console.WriteLine($"The difference of {a} - {b} is {difference}\n");
-            Console.WriteLine($"The product of {a} * {b} is {product}\n");
-            Console.WriteLine($"The quotient of {a} / {b} is {quotient}\n");
-            Console.WriteLine($"The remainder of {a} / {b} is {remainder}\n");
-            Console.WriteLine($"{a}/{b} is {quotient2} remainder {remainder2}");
+            // Ex1d. output results with formated string
+            Console.WriteLine($"\t- Exercise 1 -\n");
+            Console.WriteLine($"The sum of {a} + {b} is {sum}");
+            Console.WriteLine($"The difference of {a} - {b} is {difference}");
+            Console.WriteLine($"The product of {a} * {b} is {product}");
+            Console.WriteLine($"The quotient of {a} / {b} is {quotient}");
+            Console.WriteLine($"The remainder of {a} / {b} is {remainder}");
+            Console.WriteLine($"{a}/{b} is {quotient2} remainder {remainder2}\n");
+
+            // Ex2c
+            Console.WriteLine("\t- Exercise 2 -\n");
+            double radius = GetRadius();
+            Console.WriteLine($"\nThe area of a circle with the radius of {radius} is {AreaOfCircle(radius)}");
+            
         }
     }
 }
